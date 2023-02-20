92e7b5ae46ce9d90e91a36ccbcb7ac06b5026dca
Leonardo7Silva <107073892+Leonardo7Silva@users.noreply.github.com>
2023-02-20 11:38:24 -0300

    Delete dist directory

--- dist/config/database.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-//const prisma = new PrismaClient()
-export let prisma;
-export function connectDb() {
-    prisma = new PrismaClient();
-}
-export async function disconnectDB() {
-    await prisma?.$disconnect();
-}
